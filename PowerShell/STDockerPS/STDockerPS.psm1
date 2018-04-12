3a2bf0a0bab5b5b0ac00915f1742840eaefd9094
Joakim Svendsen <joakimbs@gmail.com>
2018-04-12 22:32:58 +0200

    2.0.7

--- PowerShell/STDockerPS/STDockerPS.psm1
+++ PowerShell/STDockerPS/STDockerPS.psm1
@@ -2,19 +2,17 @@
 function DockerPs {
 <#
 .SYNOPSIS
-    Turn "docker ps" output into custom PowerShell objects. By default omit the name column
-    (so long as it remains the last one and names can't have spaces).
+    Turn "docker ps" output into custom PowerShell objects.
 
 .DESCRIPTION
     The parsing to "objectify" relies on the column widths in this case, rather than my usual
     approach of using regular expressions, since it seems more robust for this
     case/output at hand.
 
-    Use the parameter --OmitNames (will not be tab completed), to omit the name column for the
-    containers. This is to remedy a broken part of my own brain - or for condensed output -
-    if you prefer. Just "--Omit" will work as a minimum.
+    Use the parameter -OmitNames (will not be tab completed), to omit the name column for the
+    containers. This is to get more condensed output. Just "-Omit" will work as a minimum.
     
-    Otherwise "docker ps --help" is what you should read  for further information about the
+    Otherwise, "docker ps --help" is what you should read  for further information about the
     available parameters.
 
     -q / --quiet is special-cased to work identically as in the native "docker ps -q" (lists
@@ -24,37 +22,41 @@ function DockerPs {
     Omit the container name column. Not tab completed. "-Omit" is the shortest allowed form.
 
 .EXAMPLE
-dockerps
+dockerps | select -first 3
 
+
+CONTAINER_ID : 26b08e320d17
+IMAGE        : microsoft/nanoserver
+COMMAND      : "cmd"
+CREATED      : 17 hours ago
+STATUS       : Up 17 hours
+PORTS        : 
+NAMES        : temp1234temp1234
 
 CONTAINER_ID : d92cb8edaf53
 IMAGE        : microsoft/nanoserver
 COMMAND      : "powershell"
-CREATED      : 19 hours ago
-STATUS       : Up 19 hours
+CREATED      : 39 hours ago
+STATUS       : Up 39 hours
 PORTS        : 
+NAMES        : temp3
 
 CONTAINER_ID : 9c45b67925db
 IMAGE        : microsoft/nanoserver
 COMMAND      : "powershell"
-CREATED      : 19 hours ago
-STATUS       : Up 19 hours
+CREATED      : 40 hours ago
+STATUS       : Up 40 hours
 PORTS        : 
-
-CONTAINER_ID : 520649cfb9d7
-IMAGE        : microsoft/nanoserver
-COMMAND      : "cmd"
-CREATED      : 20 hours ago
-STATUS       : Up 20 hours
-PORTS        : 
+NAMES        : temp2
 
 .EXAMPLE
 $Var = dockerps
 
-$Var.CONTAINER_ID # list all container IDs
+$Var.CONTAINER_ID # list all container IDs (but use -q for (only) that)
 $Var.COMMAND # list all commands
 $Var.STATUS # list all statuses
 $Var.IMAGE # list all image titles
+$Var.NAMES # list all container names
 
 .EXAMPLE
 PS C:\temp\STDockerPs> @(dockerps | Where { $_.IMAGE -like "microsoft/*" }).Count
@@ -65,22 +67,47 @@ PS C:\temp\STDockerPs> @(dockerps | Where { $_.IMAGE -like "microsoft/*" }).Comm
 "powershell"
 "cmd"
 
-
 .EXAMPLE
-(dockerps -names)[2].Names
-temp1
-
-.EXAMPLE
-(dockerps -names)[0]
+dockerps -omit | select -first 1
 
 
-CONTAINER_ID : d92cb8edaf53
+CONTAINER_ID : 26b08e320d17
 IMAGE        : microsoft/nanoserver
-COMMAND      : "powershell"
-CREATED      : 19 hours ago
-STATUS       : Up 19 hours
+COMMAND      : "cmd"
+CREATED      : 17 hours ago
+STATUS       : Up 17 hours
 PORTS        : 
-NAMES        : temp3
+
+.EXAMPLE
+dockerps | select -first 1
+
+
+CONTAINER_ID : 26b08e320d17
+IMAGE        : microsoft/nanoserver
+COMMAND      : "cmd"
+CREATED      : 17 hours ago
+STATUS       : Up 17 hours
+PORTS        : 
+NAMES        : temp1234temp1234
+
+
+
+.EXAMPLE
+PS C:\temp\STDockerPs> (dockerps).CONTAINER_ID
+26b08e320d17
+d92cb8edaf53
+9c45b67925db
+520649cfb9d7
+
+.EXAMPLE
+(dockerps -a).CONTAINER_ID
+26b08e320d17
+d92cb8edaf53
+9c45b67925db
+520649cfb9d7
+a0fc2bb9ad1b
+e82a27f84cb6
+
 #>  
     if ($Args -match '-q|--quiet') {
         docker ps ($Args -replace '-Omit')
