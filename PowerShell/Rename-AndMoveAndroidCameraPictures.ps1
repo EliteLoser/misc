0bf1ec70d494f00696f46337b74b644b5993e0f2
Joakim Svendsen <joakimbs@gmail.com>
2019-04-21 19:39:45 +0200

    For moving Android OneDrive-synced pictures
    Will move them into a folder appropriate for its date and tag it with datetime, something Android currently doesn't do.

---
+++ PowerShell/Rename-AndMoveAndroidCameraPictures.ps1
@@ -0,0 +1,22 @@
+[CmdletBinding()]
+Param(
+        [Switch]
+        $WhatIf = $True
+    )
+
+$FilesToMoveAndRename = Get-ChildItem -Path "$Env:UserProfile\OneDrive\Pictures\Camera Roll\IMAG????.jpg"
+
+foreach ($File in $FilesToMoveAndRename) {
+    $NewName = "$($File.LastWriteTime.ToString('yyyyMMdd'))_Android-$($File.Name)"
+    $Year = $File.LastWriteTime.Year
+    $Month = $File.LastWriteTime.Month
+    $File | Rename-Item -NewName { "$Env:UserProfile\OneDrive\Pictures\Camera Roll\$NewName" } -WhatIf:$WhatIf -Verbose
+    Write-Verbose -Verbose "Sleeping for two seconds to let the file get renamed properly..."
+    Start-Sleep -Seconds 2
+    Move-Item -LiteralPath $(if ($WhatIf) {
+        $File.FullName
+    } else {
+        "$Env:UserProfile\OneDrive\Pictures\Camera Roll\$NewName"
+    }) -Destination "$Env:UserProfile\OneDrive\Pictures\Camera Roll\$Year\$("{0:D2}" -f $Month)" -WhatIf:$WhatIf -Verbose
+}
+
