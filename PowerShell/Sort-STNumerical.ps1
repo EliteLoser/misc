8f9c485cfa82cc1c6ae0a237d8a1db3bcd448678
Joakim Svendsen <joakimbs@gmail.com>
2019-05-09 00:25:55 +0200

    Upload Sort-STNumerical.ps1
    Sorts string with numbers, and IPv4 addresses, in a human-friendly way

---
+++ PowerShell/Sort-STNumerical.ps1
@@ -0,0 +1,64 @@
+﻿function Sort-STNumerical {
+    <#
+        .SYNOPSIS
+            Sort a collection of strings containing numbers, or a mix of this and 
+            numerical data types - in a human-friendly way.
+
+            This will sort "anything" you throw at it correctly.
+
+            Author: Joakim Borger Svendsen, Copyright 2019-present, Svendsen Tech.
+
+            MIT License
+
+        .PARAMETER InputObject
+            Collection to sort.
+
+        .PARAMETER MaximumDigitCount
+            Maximum numbers of digits to account for in a row, in order for them to be sorted
+            correctly. Default: 100. This is the .NET framework maximum as of 2019-05-09.
+            For IPv4 addresses "3" is sufficient, but "overdoing" does no or little harm. It might
+            eat some more resources, which can matter on really huge files/data sets.
+
+        .EXAMPLE
+            $Strings | Sort-STNumerical
+
+            Sort strings containing numbers in a way that magically makes them sorted human-friendly
+            
+        .EXAMPLE
+            $Result = Sort-STNumerical -InputObject $Numbers
+            $Result
+
+            Sort numbers in a human-friendly way.
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(
+            Mandatory = $True,
+            ValueFromPipeline = $True,
+            ValueFromPipelineBypropertyName = $True)]
+        [System.Object[]]
+        $InputObject,
+        
+        [ValidateRange(2, 100)]
+        [Byte]
+        $MaximumDigitCount = 100)
+    
+    Begin {
+        [System.Object[]] $InnerInputObject = @()
+    }
+    
+    Process {
+        $InnerInputObject += $InputObject
+    }
+
+    End {
+        $InnerInputObject |
+            Sort-Object -Property `
+                @{ Expression = {
+                    [Regex]::Replace($_, '(\d+)', {
+                        "{0:D$MaximumDigitCount}" -f [Int] $Args[0].Value })
+                    }
+                },
+                @{ Expression = { $_ } }
+    }
+}
