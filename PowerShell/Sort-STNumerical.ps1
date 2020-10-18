ced8282d897085c1d485f34ea6a4b3c2c7caacd7
Matthew MacFarland <macfarmw@gmail.com>
2020-10-18 14:36:49 -0500

    Update Sort-STNumerical.ps1
    Add a Descending switch to override the default ascending sort when needed.

--- PowerShell/Sort-STNumerical.ps1
+++ PowerShell/Sort-STNumerical.ps1
@@ -1,4 +1,4 @@
-﻿function Sort-STNumerical {
+function Sort-STNumerical {
     <#
         .SYNOPSIS
             Sort a collection of strings containing numbers, or a mix of this and 
@@ -12,12 +12,15 @@
 
         .PARAMETER InputObject
             Collection to sort.
-
+        
         .PARAMETER MaximumDigitCount
             Maximum numbers of digits to account for in a row, in order for them to be sorted
             correctly. Default: 100. This is the .NET framework maximum as of 2019-05-09.
             For IPv4 addresses "3" is sufficient, but "overdoing" does no or little harm. It might
             eat some more resources, which can matter on really huge files/data sets.
+
+        .PARAMETER Descending
+            Optional switch to sort in descending order rather than the default ascending order.
 
         .EXAMPLE
             $Strings | Sort-STNumerical
@@ -29,22 +32,35 @@
             $Result
 
             Sort numbers in a human-friendly way.
+
+        .EXAMPLE
+            @("1.1.0","1.1.11","1.1.2") | Sort-STNumerical -Descending
+
+            1.1.11, 1.1.2, 1.1.0
     #>
     [CmdletBinding()]
     Param(
         [Parameter(
             Mandatory = $True,
             ValueFromPipeline = $True,
-            ValueFromPipelineByPropertyName = $True)]
+            ValueFromPipelineBypropertyName = $True)]
         [System.Object[]]
         $InputObject,
         
         [ValidateRange(2, 100)]
         [Byte]
-        $MaximumDigitCount = 100)
+        $MaximumDigitCount = 100,
+
+        [Switch]$Descending
+    )
     
     Begin {
         [System.Object[]] $InnerInputObject = @()
+        
+        [bool]$desc = $false
+        if($Descending){
+            $desc = $true
+        }
     }
     
     Process {
@@ -56,9 +72,9 @@
             Sort-Object -Property `
                 @{ Expression = {
                     [Regex]::Replace($_, '(\d+)', {
-                        "{0:D$MaximumDigitCount}" -f [Int] $Args[0].Value })
+                        "{0:D$MaximumDigitCount}" -f [Int64] $Args[0].Value })
                     }
                 },
-                @{ Expression = { $_ } }
+                @{ Expression = { $_ } } -Descending:$desc
     }
 }
