ea22bc81a08781b5691ae11476f93a8d5b70274b
Joakim Svendsen <joakimbs@gmail.com>
2020-11-19 22:23:45 +0100

    Better, support adding more recent data, etc.

--- PowerShell/Get-AdSenseReport.ps1
+++ PowerShell/Get-AdSenseReport.ps1
@@ -1,48 +1,46 @@
+[CmdletBinding()]
+Param()
 
+$Path = './adsense report2.csv'
+$CsvData = @(Import-Csv -LiteralPath $Path)
 
-$CsvDataFormatted = Import-Csv -Path './adsense report.csv' |
+# Add recent data.
+$LastDate = [DateTime]($CsvData[-1].Dato)
+Write-Verbose "Last date in main report: $LastDate" -Verbose
+
+if (Test-Path -LiteralPath ($Path = './adsense-new.csv')) {
+    $CsvData += @(Import-Csv -LiteralPath $Path).Where({([DateTime] $_.Dato) -gt $LastDate}) |
+        Sort-Object -Descending
+}
+
+$LastDate = [DateTime]($CsvData[-1].Dato)
+Write-Verbose "Last date in report after adding potentially new data: $LastDate" -Verbose
+
+# Format the data.
+$CsvDataFormatted = $CsvData |
     ForEach-Object {
         Add-Member -InputObject $_ -MemberType NoteProperty -Name Dato -Value ([DateTime]$_.Dato) -Force
-        Add-Member -InputObject $_ -MemberType NoteProperty -Name Year -Value $_.Dato.Year
-        Add-Member -InputObject $_ -MemberType NoteProperty -Name YearAndMonth -Value ([String]$_.Dato.Year + "-" + ("{0:D2}" -f $_.Dato.Month))
-        Add-Member -InputObject $_ -MemberType NoteProperty -Name Income -Value ([Decimal]$_."Anslåtte inntekter (NOK)" -replace ',', '.') -PassThru
+        Add-Member -InputObject $_ -MemberType NoteProperty -Name Year -Value $_.Dato.Year -Force
+        Add-Member -InputObject $_ -MemberType NoteProperty -Name YearAndMonth -Value ([String]$_.Dato.Year + "-" + ("{0:D2}" -f $_.Dato.Month)) -Force
+        Add-Member -InputObject $_ -MemberType NoteProperty `
+            -Name Income -Value ([Decimal]$_."Anslåtte inntekter (NOK)" -replace ',', '.') -Force -PassThru
     } |
     Sort-Object -Property Dato -Descending
 
-#$Data = @{}
 
 # Monthly
-
 $CsvDataFormatted |
     Group-Object -Property YearAndMonth |
     ForEach-Object {
         [PSCustomObject] @{
             YearAndMonth = $_.Name
             MonthlyIncome = ($_.Group | Measure-Object -Property Income -Sum).Sum
-        } | Select-Object -Property YearAndMonth, MonthlyIncome, Year, YearlyIncome
+        } |
+            # This is to get all the properties displayed at the end.
+            Select-Object -Property YearAndMonth, MonthlyIncome, Year, YearlyIncome
     }
 
-<#
-$CsvDataFormatted |
-    ForEach-Object { 
-        if (++$Counter -eq 1) { 
-            $LastMonth = "{0:D2}" -f $_.Dato.Month
-        }
-        if ($LastMonth -eq $_.Dato.Month) {
-            $Data.([String]$_.Dato.Year + "-" + $LastMonth) += $_.Income
-        }
-        else {
-            $LastMonth = "{0:D2}" -f $_.Dato.Month
-            $Data.([String]$_.Dato.Year + "-" + $LastMonth) += $_.Income
-        }
-    }
-
-$Data.GetEnumerator() | Sort-Object -Property Name | Format-Table -AutoSize
-
-$Data = @{}
-#>
 # Yearly
-
 $CsvDataFormatted |
     Group-Object -Property Year |
     ForEach-Object {
@@ -51,4 +49,3 @@ $CsvDataFormatted |
             YearlyIncome = ($_.Group | Measure-Object -Property Income -Sum).Sum
         } 
     }
-    
