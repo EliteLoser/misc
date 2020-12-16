e171d6c31fe4ea89d19771f0f39a61e45e934523
Joakim Svendsen <joakimbs@gmail.com>
2020-12-16 13:59:17 +0100

    Create IdioticWebAccessCheck.ps1

---
+++ PowerShell/IdioticWebAccessCheck.ps1
@@ -0,0 +1,22 @@
+if ($PSVersionTable.Version.Major -gt 5 -and -not $IsWindows) {
+    Write-Error "Only works on Windows." -ErrorAction Stop
+}
+
+# You need to populate this with remote IPs or host names
+# $Servers = @('server1', 'server2')
+
+Invoke-Command -ComputerName $Servers -ScriptBlock {
+    $CanReachWeb = if (((Invoke-WebRequest -UseBasicParsing -Uri 'http://microsoft.com') |
+        Select-Object -ExpandProperty Links |
+        Select-Object -ExpandProperty href) -match 'https://choice.microsoft.com') { 
+            $True
+        } else { 
+            $False
+        }
+    [PSCustomObject]@{
+        ComputerName = $Env:ComputerName
+        Domain = Get-WmiObject -Class Win32_ComputerSystem -Property Domain |
+            Select-Object -ExpandProperty Domain -ErrorAction SilentlyContinue
+        CanReachWeb = $CanReachWeb
+    }
+}
