b35e066f7e5446be05c5210e1988048303987150
Joakim Svendsen <joakimbs@gmail.com>
2021-09-02 19:00:15 +0200

    Create Sorting Computer Names.ps1

---
+++ PowerShell/Sorting Computer Names.ps1
@@ -0,0 +1,6 @@
+$Servers | Sort @{ Expression = { $_ -replace '^[^.]+\.' } },
+    @{ Expression = { [regex]::Replace($_.Split('.')[0], '(\d+)', { '{0:D16}' -f [int] $args[0].Value }) } },
+    @{ Expression = { $_ } }
+    
+# This will sort correctly with numbers in strings (when not zero-padded), by domain first, then host name.
+# All in a single pipeline for delicious consumption.
